9eb48445122cc29d4aae646d8d4dc3d2e301f01e
Dmitry Shychkov <Dmitry_Shychkov@mail.ru>
2025-01-31 14:37:14 +0300

    Using a timer asynchronously

--- TestBoostAsio/main.cpp
+++ TestBoostAsio/main.cpp
@@ -1,26 +1,22 @@
-﻿#include <iostream>
+﻿#include <functional>
+#include <iostream>
 #include <boost/asio.hpp>
 
-#include "client.h"
-#include "server.h"
-
-namespace {
-	constexpr auto ServerApplication = "server";
-	constexpr auto ClientApplication = "client";
+namespace
+{
+	void print(const boost::system::error_code& /*e*/)
+	{
+		std::cout << "Hello, world!" << std::endl;
+	}
 }
 
 int main(int argc, char** argv)
 {
-	if (argc != 2)
-	{
-		std::cout << "Choose client or server!" << std::endl;
-		return 1;
-	}
-
-	using asio = boost::asio;
+	namespace asio = boost::asio;
 
 	asio::io_context io;
 	asio::steady_timer t(io, asio::chrono::seconds(5));
-	t.wait();
+	t.async_wait(&print);
+	io.run();
 	return 0;
 }
