1b1c4a9c9b18ae428547bdf01a5930b78a794cb0
Dmitry Shychkov <Dmitry_Shychkov@mail.ru>
2025-01-31 15:38:36 +0300

    Binding arguments to a completion handler

--- TestBoostAsio/main.cpp
+++ TestBoostAsio/main.cpp
@@ -4,9 +4,16 @@
 
 namespace
 {
-	void print(const boost::system::error_code& /*e*/)
+	void print(const boost::system::error_code& /*e*/, boost::asio::steady_timer* t, int* count)
 	{
-		std::cout << "Hello, world!" << std::endl;
+		if (*count < 5)
+		{
+			std::cout << *count << std::endl;
+			++(*count);
+
+			t->expires_at(t->expiry() + boost::asio::chrono::seconds(1));
+			t->async_wait(std::bind(print,	boost::asio::placeholders::error, t, count));
+		}
 	}
 }
 
@@ -15,8 +22,10 @@ int main(int argc, char** argv)
 	namespace asio = boost::asio;
 
 	asio::io_context io;
-	asio::steady_timer t(io, asio::chrono::seconds(5));
-	t.async_wait(&print);
+	int count = 0;
+	asio::steady_timer t(io, asio::chrono::seconds(1));
+	t.async_wait(std::bind(print, asio::placeholders::error, &t, &count));
 	io.run();
+	std::cout << "Final count is " << count << std::endl;
 	return 0;
 }
