64b32bf64fca68d4c1d81b2eb2770e6623c62b39
Dmitry Shychkov <Dmitry_Shychkov@mail.ru>
2025-01-31 19:29:36 +0300

    Using a member function as a completion handler

--- TestBoostAsio/main.cpp
+++ TestBoostAsio/main.cpp
@@ -4,28 +4,53 @@
 
 namespace
 {
-	void print(const boost::system::error_code& /*e*/, boost::asio::steady_timer* t, int* count)
-	{
-		if (*count < 5)
-		{
-			std::cout << *count << std::endl;
-			++(*count);
+    class Printer
+    {
+    public:
+        Printer(boost::asio::io_context& io)
+            : timer_(io, boost::asio::chrono::seconds(1)),
+            count_(0)
+        {
+            timer_.async_wait(std::bind(&Printer::print, this));
+        }
 
-			t->expires_at(t->expiry() + boost::asio::chrono::seconds(1));
-			t->async_wait(std::bind(print,	boost::asio::placeholders::error, t, count));
-		}
-	}
+        ~Printer()
+        {
+            std::cout << "Final count is " << count_ << std::endl;
+        }
+
+        void print()
+        {
+            if (count_ < 5)
+            {
+                std::cout << "Printer: " << count_ << std::endl;
+                ++count_;
+
+                timer_.expires_after(boost::asio::chrono::seconds(1));
+                timer_.async_wait(std::bind(&Printer::print, this));
+                std::cout << "Another code..." << std::endl;
+            }
+            else
+            {
+                std::cout << "Canceling timer..." << std::endl;
+                timer_.cancel();
+                static_cast<boost::asio::io_context&>(timer_.get_executor().context()).stop();
+            }
+        }
+
+    private:
+        boost::asio::steady_timer timer_;
+        int count_;
+    };
 }
 
 int main(int argc, char** argv)
 {
 	namespace asio = boost::asio;
 
-	asio::io_context io;
-	int count = 0;
-	asio::steady_timer t(io, asio::chrono::seconds(1));
-	t.async_wait(std::bind(print, asio::placeholders::error, &t, &count));
-	io.run();
-	std::cout << "Final count is " << count << std::endl;
+    asio::io_context io;
+    Printer p(io);
+    io.run();
+    std::cout << "Bye" << std::endl;
 	return 0;
 }
