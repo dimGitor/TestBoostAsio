0f5cf4c9ee96905108b0a4d4d9df5a80fce84b3d
Dmitry Shychkov <Dmitry_Shychkov@mail.ru>
2025-02-01 21:42:04 +0300

    Syntax refactoring

--- TestBoostAsio/main.cpp
+++ TestBoostAsio/main.cpp
@@ -20,171 +20,171 @@ namespace {
 #else
         localtime_r(&now, &localTime);
 #endif
-
-        oss << std::put_time(&localTime, "%Y-%m-%d %H:%M:%S"); // Format: YYYY-MM-DD HH:MM:SS
+        // Format: YYYY-MM-DD HH:MM:SS
+        oss << std::put_time(&localTime, "%Y-%m-%d %H:%M:%S");
         return oss.str();
     }
 
     constexpr auto IP_ADDRESS = "localhost";
-} // namespace
 
-class tcp_connection
-  : public std::enable_shared_from_this<tcp_connection>
-{
-public:
-    typedef std::shared_ptr<tcp_connection> pointer;
-
-    static pointer create(boost::asio::io_context& io_context)
+    class tcp_connection: public std::enable_shared_from_this<tcp_connection>
     {
-        return pointer(new tcp_connection(io_context));
-    }
+    public:
+        typedef std::shared_ptr<tcp_connection> pointer;
 
-    tcp::socket& socket()
+        static pointer create(boost::asio::io_context& io_context)
+        {
+            return pointer(new tcp_connection(io_context));
+        }
+
+        tcp::socket& socket()
+        {
+            return socket_;
+        }
+
+    void start()
     {
-        return socket_;
-    }
-
-  void start()
-  {
         message_ = make_daytime_string();
 
         boost::asio::async_write(socket_, boost::asio::buffer(message_),
             std::bind(&tcp_connection::handle_write, shared_from_this()));
-  }
-
-private:
-    tcp_connection(boost::asio::io_context& io_context)
-        : socket_(io_context)
-    {
-    }
-
-    void handle_write()
-    {
-    }
-
-    tcp::socket socket_;
-    std::string message_;
-};
-
-class tcp_server
-{
-public:
-    tcp_server(boost::asio::io_context& io_context)
-        : io_context_(io_context),
-        acceptor_(io_context, tcp::endpoint(tcp::v4(), 13))
-    {
-        start_accept();
     }
 
     private:
-    void start_accept()
-    {
-        tcp_connection::pointer new_connection =
-        tcp_connection::create(io_context_);
-
-        acceptor_.async_accept(new_connection->socket(),
-            std::bind(&tcp_server::handle_accept, this, new_connection,
-            boost::asio::placeholders::error));
-    }
-
-    void handle_accept(tcp_connection::pointer new_connection,
-        const boost::system::error_code& error)
-    {
-        if (!error)
+        tcp_connection(boost::asio::io_context& io_context): socket_(io_context)
         {
-        new_connection->start();
         }
 
-        start_accept();
-    }
-
-    boost::asio::io_context& io_context_;
-    tcp::acceptor acceptor_;
-};
-
-class udp_server
-{
-public:
-    udp_server(boost::asio::io_context& io_context)
-        : socket_(io_context, udp::endpoint(udp::v4(), 13))
-    {
-        start_receive();
-    }
-
-private:
-    void start_receive()
-    {
-        socket_.async_receive_from(
-            boost::asio::buffer(recv_buffer_), remote_endpoint_,
-            std::bind(&udp_server::handle_receive, this,
-            boost::asio::placeholders::error));
-    }
-
-    void handle_receive(const boost::system::error_code& error)
-    {
-        if (!error)
+        void handle_write()
         {
-        std::shared_ptr<std::string> message(
-            new std::string(make_daytime_string()));
+        }
 
-        socket_.async_send_to(boost::asio::buffer(*message), remote_endpoint_,
-            std::bind(&udp_server::handle_send, this, message));
+        tcp::socket socket_;
+        std::string message_;
+    };
 
-        start_receive();
+    class tcp_server
+    {
+    public:
+        tcp_server(boost::asio::io_context& io_context)
+            : io_context_(io_context),
+            acceptor_(io_context, tcp::endpoint(tcp::v4(), 13))
+        {
+            start_accept();
+        }
+
+    private:
+        void start_accept()
+        {
+            tcp_connection::pointer new_connection = tcp_connection::create(io_context_);
+
+            acceptor_.async_accept(new_connection->socket(),
+                std::bind(&tcp_server::handle_accept, this, new_connection,
+                boost::asio::placeholders::error));
+        }
+
+        void handle_accept(tcp_connection::pointer new_connection, const boost::system::error_code& error)
+        {
+            if (!error)
+            {
+                std::cout << "tcp_server::handle_accept" << std::endl;
+                new_connection->start();
+            }
+
+            start_accept();
+        }
+
+    private:
+        boost::asio::io_context& io_context_;
+        tcp::acceptor acceptor_;
+    };
+
+    class udp_server
+    {
+    public:
+        udp_server(boost::asio::io_context& io_context)
+            : socket_(io_context, udp::endpoint(udp::v4(), 13))
+        {
+            start_receive();
+        }
+
+    private:
+        void start_receive()
+        {
+            socket_.async_receive_from(
+                boost::asio::buffer(recv_buffer_), remote_endpoint_,
+                std::bind(&udp_server::handle_receive, this,
+                boost::asio::placeholders::error));
+        }
+
+        void handle_receive(const boost::system::error_code& error)
+        {
+            if (!error)
+            {
+                std::cout << "udp_server::handle_receive" << std::endl;
+                std::shared_ptr<std::string> message(
+                    new std::string(make_daytime_string()));
+
+                socket_.async_send_to(boost::asio::buffer(*message), remote_endpoint_,
+                    std::bind(&udp_server::handle_send, this, message));
+
+                start_receive();
+            }
+        }
+
+        void handle_send(std::shared_ptr<std::string> /*message*/)
+        {
+        }
+
+    private:
+        udp::socket socket_;
+        udp::endpoint remote_endpoint_;
+        std::array<char, 1> recv_buffer_;
+    };
+
+    void clientWorker()
+    {
+        try
+        {
+            boost::asio::io_context io_context;
+
+            udp::resolver resolver(io_context);
+            udp::endpoint receiver_endpoint =
+            *resolver.resolve(udp::v4(), IP_ADDRESS, "daytime").begin();
+
+            udp::socket socket(io_context);
+            socket.open(udp::v4());
+
+            std::array<char, 1> send_buf  = {{ 0 }};
+            socket.send_to(boost::asio::buffer(send_buf), receiver_endpoint);
+
+            std::array<char, 128> recv_buf;
+            udp::endpoint sender_endpoint;
+            size_t len = socket.receive_from(boost::asio::buffer(recv_buf), sender_endpoint);
+
+            std::cout.write(recv_buf.data(), len);
+        }
+        catch (std::exception& e)
+        {
+            std::cerr << e.what() << std::endl;
         }
     }
 
-    void handle_send(std::shared_ptr<std::string> /*message*/)
+    void serverWorker()
     {
+        try
+        {
+            boost::asio::io_context io_context;
+            tcp_server server1(io_context);
+            udp_server server2(io_context);
+            io_context.run();
+        }
+        catch (std::exception& e)
+        {
+            std::cerr << e.what() << std::endl;
+        }
     }
-
-    udp::socket socket_;
-    udp::endpoint remote_endpoint_;
-    std::array<char, 1> recv_buffer_;
-};
-
-void clientWorker()
-{
-    try
-    {
-        boost::asio::io_context io_context;
-
-        udp::resolver resolver(io_context);
-        udp::endpoint receiver_endpoint =
-        *resolver.resolve(udp::v4(), IP_ADDRESS, "daytime").begin();
-
-        udp::socket socket(io_context);
-        socket.open(udp::v4());
-
-        std::array<char, 1> send_buf  = {{ 0 }};
-        socket.send_to(boost::asio::buffer(send_buf), receiver_endpoint);
-
-        std::array<char, 128> recv_buf;
-        udp::endpoint sender_endpoint;
-        size_t len = socket.receive_from(boost::asio::buffer(recv_buf), sender_endpoint);
-
-        std::cout.write(recv_buf.data(), len);
-    }
-    catch (std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-}
-
-void serverWorker()
-{
-    try
-    {
-        boost::asio::io_context io_context;
-        tcp_server server1(io_context);
-        udp_server server2(io_context);
-        io_context.run();
-    }
-    catch (std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-}
+} // namespace
 
 int main(int argc, char* argv[])
 {
